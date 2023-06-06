4e76510e4c39d4c968f987a39dc9c5513697054f
Nadya Mincheva <7000585m1@gmail.com>
2023-06-06 12:49:25 +0300

    start project

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/584999ba7b7d4d76317f6001.png')}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2f4f4f',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#8b0000',
+    fontWeight: 'bold',
+    fontSize: 30,
+  }
 });
